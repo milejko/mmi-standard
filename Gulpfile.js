c918111a0286141adcf5b0c0a89e1463951a4996
Maciej Pawłowski <mac@macu.eu>
2015-09-24 11:31:53 +0200

    task DbDeploy without args

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var exec = require('gulp-exec');
 
-gulp.task('default', function () {
-	
+gulp.task('dbdeploy', function () {
+	var options = {
+		continueOnError: false, // default = false, true means don't emit error event 
+		pipeStdout: false, // default = false, true means stdout is written to file.contents 
+		enviroment: 'PROD'
+	};
+	var reportOptions = {
+		err: true, // default = true, false means don't write err 
+		stderr: true, // default = true, false means don't write stderr 
+		stdout: true // default = true, false means don't write stdout 
+	};
+	gulp.src('.')
+			.pipe(exec('php vendor/mmi/mmi/src/Mmi/Command/DbDeploy.php <%= options.enviroment %>', options))
+			.pipe(exec.reporter(reportOptions));
 });
