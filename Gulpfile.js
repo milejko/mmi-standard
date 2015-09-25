56f77ef41fb4142605479b46a024b47f4c70fb87
Maciej Pawłowski <mac@macu.eu>
2015-09-25 13:59:55 +0200

    gulp daorender task

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,19 +7,28 @@ var env = 'DEV';
 if (argv.e !== undefined) {
 	env = argv.e;
 }
+var options = {
+	continueOnError: false, // default = false, true means don't emit error event 
+	pipeStdout: false, // default = false, true means stdout is written to file.contents 
+	enviroment: env
+};
+var reportOptions = {
+	err: true, // default = true, false means don't write err 
+	stderr: true, // default = true, false means don't write stderr 
+	stdout: true // default = true, false means don't write stdout 
+};
+
+gulp.task('default', ['dbdeploy', 'daorender'], function () {
+});
 
 gulp.task('dbdeploy', function () {
-	var options = {
-		continueOnError: false, // default = false, true means don't emit error event 
-		pipeStdout: false, // default = false, true means stdout is written to file.contents 
-		enviroment: env
-	};
-	var reportOptions = {
-		err: true, // default = true, false means don't write err 
-		stderr: true, // default = true, false means don't write stderr 
-		stdout: true // default = true, false means don't write stdout 
-	};
 	gulp.src('.')
 			.pipe(exec('php vendor/mmi/mmi/src/Mmi/Command/DbDeploy.php <%= options.enviroment %>', options))
+			.pipe(exec.reporter(reportOptions));
+});
+
+gulp.task('daorender', function () {
+	gulp.src('.')
+			.pipe(exec('php vendor/mmi/mmi/src/Mmi/Command/DaoRender.php <%= options.enviroment %>', options))
 			.pipe(exec.reporter(reportOptions));
 });
