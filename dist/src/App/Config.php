508a339d55bd8beb443642f2dd4a73127f782ea2
Mariusz Miłejko <mariusz@milejko.pl>
2015-10-02 08:22:12 +0200

    Upgraded dist

---
+++ dist/src/App/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ * 
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2014 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+namespace App;
+
+/**
+ * Ogólna konfiguracja aplikacji
+ */
+class Config extends \Mmi\App\KernelConfig {
+
+	/**
+	 * Konstruktor
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+
+}
