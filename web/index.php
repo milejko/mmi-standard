254a6a225120ce496cd17a4f7a81c4876ad1b0ad
Mariusz Milejko <mariusz@milejko.pl>
2020-11-10 00:21:29 +0100

    mmi 4.0

---
+++ web/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Mmi Framework (https://bitbucket.org/mariuszmilejko/mmicms/)
+ *
+ * @link       https://bitbucket.org/mariuszmilejko/mmicms/
+ * @copyright  Copyright (c) 2010-2020 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+use Mmi\App\App;
+
+//define base path
+define('BASE_PATH', realpath(__DIR__ . '/../'));
+
+//autoloader definition
+require BASE_PATH . '/vendor/autoload.php';
+
+//run application
+(new App())->run();
